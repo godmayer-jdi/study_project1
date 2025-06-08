cebb66313d7b2ab13d62766a286261311bf58868
unknown <jordi5@bk.ru>
2025-06-08 15:13:28 +0400

    Добавлены тесты для модуля widget

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,9 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 print("Маскировка карты:", get_mask_card_number("7000792289606361"))  # 7000 79** **** 6361
 print("Маскировка счета:", get_mask_account("73654108430135874305"))  # **4305
+
+print("Маскировка информации о картах и счетах:", mask_account_card("Maestro 1596837868705199"))
+# Maestro 1596 83** **** 5199
+print("Изменение формата даты:", get_date("2024-03-11T02:26:18.671407"))  # 11.03.2024
