7bf54d4abf86020f9098d719e18c8ec2283b3f09
godmayer-jdi <tig774ra@gmail.com>
2025-06-29 18:05:23 +0400

    Добавлены функции transaction_descriptions b card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -13,4 +13,14 @@ def filter_by_currency(transactions: List[Dict[str, Any]], currency_code: str) -
             transaction["operationAmount"]["currency"]["code"] == currency_code
         ):
             yield transaction
-
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Iterator[str]:
+    """Генератор, который поочередно возвращает описание каждой транзакции."""
+    for transaction in transactions:
+        yield transaction.get("description", "")
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    """Генератор номеров карт в формате XXXX XXXX XXXX XXXX.
+    Генерирует номера от start до stop (не включая stop) """
+    for num in range(start, stop):
+        card_str = str(num).zfill(16)  # дополняем нулями слева до 16 символов
+        formatted = f"{card_str[0:4]} {card_str[4:8]} {card_str[8:12]} {card_str[12:16]}"
+        yield formatted
