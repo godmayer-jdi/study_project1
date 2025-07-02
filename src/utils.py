37e439b639348323e3162f815e76f71e83ead90a
godmayer-jdi <tig774ra@gmail.com>
2025-07-02 23:23:05 +0400

    добавлен модель utils с функцией принимающей на вход путь до JSON-файла и возвращающей список словарей с данными о финансовых транзакциях

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import json
+from typing import List, Dict, Any
+
+def load_transactions_from_json(path: str) -> List[Dict[str, Any]]:
+    """
+    Загружает список транзакций из JSON-файла.
+    Если файл пустой, не найден или не содержит список — возвращает пустой список.
+    """
+    try:
+        with open(path, encoding="utf-8") as f:
+            data = json.load(f)
+            if isinstance(data, list):
+                return data
+            else:
+                return []
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
